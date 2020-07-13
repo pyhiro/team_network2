89ca1a2b5deb7795cbb0f230a5bd825a504f1212
hiro <thwkana@gmail.com>
2020-07-13 12:10:06 +0900

    final

--- HandleTCPClient.c
+++ HandleTCPClient.c
@@ -13,92 +13,11 @@ void HandleTCPClient(int clntSocket)
 {
   char echoBuffer[RCVBUFSIZE];  
   int recvMsgSize;  
-
-  for (int i=0; i<50; i++) {
-    echoBuffer[i] = '\0';
-  }
-
-  if ((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) < 0)
-    DieWithError("recv() failed");
   
-  if (!strcmp(echoBuffer, "anagram")) {
-    if (recvMsgSize % 2 == 0) {
-      printf("received: %s\n", echoBuffer);
-      
-      for (int i=0; i < recvMsgSize/2; i++) {
-        tmp = echoBuffer[i];
-        echoBuffer[i] = echoBuffer[recvMsgSize-1-i]; 
-        echoBuffer[recvMsgSize-1-i] = tmp;
-      } 
-    } else {
-      printf("received: %s\n", echoBuffer);
-      for (int i=0; i <= recvMsgSize/2; i++) {
-        tmp = echoBuffer[i];
-        echoBuffer[i] = echoBuffer[recvMsgSize-1-i]; 
-        echoBuffer[recvMsgSize-1-i] = tmp;
-      } 
-    }
-  } else if (!strcmp(echoBuffer, "age")) {
-    printf("received: %s\n", echoBuffer);
-    strcpy(echoBuffer, "25歳です。");  
-  } else if (!strcmp(echoBuffer, "time")) {
-    char s[256];
-    time_t timer;
-    struct tm *timeptr;
-
-    timer = time(NULL);
-    timeptr = localtime(&timer);
-    strftime(s, 256, "%Y年%m月%d日%H時%M分", timeptr);
-    printf("received: %s\n", echoBuffer);
-    strcpy(echoBuffer, s);  
-  } else if (!strcmp(echoBuffer, "萩原先生は")) {
-    printf("received: %s\n", echoBuffer);
-    strcpy(echoBuffer, "優しい");  
-  } else if (!strcmp(echoBuffer, "好きな言語は")) {
-    printf("received: %s\n", echoBuffer);
-    strcpy(echoBuffer, "python");  
-  } else if (!strcmp(echoBuffer, "今の時刻は")) {
-    char s[256];
-    time_t timer;
-    struct tm *timeptr;
-
-    timer = time(NULL);
-    timeptr = localtime(&timer);
-    strftime(s, 256, "%Y年%m月%d日%H時%M分", timeptr);
-    printf("received: %s\n", echoBuffer);
-    strcpy(echoBuffer, s);  
-  } else if (!strcmp(echoBuffer, "住所は")) {
-    for (int i=0; i<BUFSIZ; i++) {
-      echoBuffer[i] = '\0';
-    }
-    printf("received: %s\n", echoBuffer);
-    strcpy(echoBuffer, "新小岩");  
-  } else if (!strcmp(echoBuffer, "c言語は")) {
-    for (int i=0; i<BUFSIZ; i++) {
-      echoBuffer[i] = '\0';
-    }
-    printf("received: %s\n", echoBuffer);
-    strcpy(echoBuffer, "嫌い");  
-  } else if (!strcmp(echoBuffer, "hello")) {
-    printf("received: %s\n", echoBuffer);
-    if (rand() % 2 == 0) {
-      strcpy(echoBuffer, "world");  
-    } else {
-      strcpy(echoBuffer, "hello");  
-    }
-  } else if (!strcmp(echoBuffer, "squit")) {
-    close(clntSocket);    
-  } else {
-      printf("received: %s\n", echoBuffer);
-      strcpy(echoBuffer, "I don't know!");
-    }
-
+  memset(&echoBuffer[0], '\0', 50);
+  
   while (recvMsgSize > 0) 
   {
-    send(clntSocket, echoBuffer, 30, 0);
-    for (int i=0; i<=256; i++) { 
-      echoBuffer[i] = '\0'; 
-    }
     if ((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) < 0)
       DieWithError("recv() failed");
 
@@ -149,15 +68,9 @@ void HandleTCPClient(int clntSocket)
       printf("received: %s\n", echoBuffer);
       strcpy(echoBuffer, s);  
     } else if (!strcmp(echoBuffer, "住所は")) {
-      for (int i=0; i<BUFSIZ; i++) {
-        echoBuffer[i] = '\0';
-      }
       printf("received: %s\n", echoBuffer);
       strcpy(echoBuffer, "新小岩");  
     } else if (!strcmp(echoBuffer, "c言語は")) {
-      for (int i=0; i<BUFSIZ; i++) {
-        echoBuffer[i] = '\0';
-      }
       printf("received: %s\n", echoBuffer);
       strcpy(echoBuffer, "嫌い");  
     } else if (!strcmp(echoBuffer, "hello")) {
@@ -168,11 +81,13 @@ void HandleTCPClient(int clntSocket)
         strcpy(echoBuffer, "hello");  
       }
     } else if (!strcmp(echoBuffer, "squit")) {
-      close(clntSocket);    
+      exit(0);
     } else {
         printf("received: %s\n", echoBuffer);
         strcpy(echoBuffer, "I don't know!");
       }
+    send(clntSocket, echoBuffer, 30, 0);
+    memset(&echoBuffer[0], '\0', 50);
   }
 
   close(clntSocket);  
